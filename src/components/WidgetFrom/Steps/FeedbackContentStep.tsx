5979583ddc3a341107c6bb6ef854d008bdfa2c5d
daniell0l <daniel_l0l@hotmail.com>
2022-05-05 17:47:44 -0300

    create button

--- src/components/WidgetFrom/Steps/FeedbackContentStep.tsx
+++ src/components/WidgetFrom/Steps/FeedbackContentStep.tsx
@@ -1,5 +1,55 @@
-export function FeedbackContentStep() {
-    return(
-        
-    )
+import { ArrowLeft, Camera } from "phosphor-react";
+import { FeedbackType, feedbackTypes } from "../..";
+import { CloseButton } from "../../CloseButton";
+import { ScreenshotButton } from "../Screenshotbutton";
+
+interface FeedbackContentStepProps {
+    feedbackType: FeedbackType;
+    onFeedbackRestartRequested: () => void;
 }
+
+export function FeedbackContentStep({
+    feedbackType, 
+    onFeedbackRestartRequested }: 
+    FeedbackContentStepProps ) {
+        const feedbackTypeInfo = feedbackTypes[feedbackType];
+        
+        return(
+            <>
+            <header>
+            <button type="button" 
+            className="top-5 left-5 absolute text-zinc-400 hover:text-zinc-100"
+            onClick={onFeedbackRestartRequested}
+            >
+            <ArrowLeft weight="bold" className="w-4 h-4" />
+            </button>
+            
+            <span className="text-xl leading-6 flex items-center gap-2">
+            <img src={feedbackTypeInfo.image.source} alt={feedbackTypeInfo.image.alt} className="w-6 h-6" />
+            {feedbackTypeInfo.title}
+            </span>
+            
+            <CloseButton />
+            </header>
+            
+            <form className="my-4 w-full">
+            <textarea
+            className="min-w-[304px] w--full min-h-[112px] text-sm placeholder-zinc-400 border-zinc-600 bg-transparent rounded-md focus:border-brand-500 focus:ring-brand-500 focus:ring-1 focus:outline-none resize-nome scrollbar scrollbar-thumb-zinc-700 scrollbar-track-transparent scrollbar-thin"
+            placeholder="Conte com detalhes o que está acontecendo..."
+            />
+            
+            <footer className="flex gap-2 mt-2">
+
+                <ScreenshotButton />
+
+            <button
+            type="submit"
+            className="p-2 bg-brand-500 rounded-md border-transparent flex-1 flex justify-center items-center text-sm hover:bg-brand-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500 transition-colors"
+            >
+            Enviar feedback
+            </button>
+            </footer>
+            </form>
+            </>
+            );
+        }
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,11 +1,9 @@
-import { useState } from "react";
-
-import { CloseButton } from "./CloseButton";
-
 import bugImageUrl from '../assets/bug.svg'
 import ideaImageUrl from '../assets/idea.svg'
 import thoughtImageUrl from '../assets/thought.svg'
 import { FeedbackTypeStep } from "./WidgetFrom/Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./WidgetFrom/Steps/FeedbackContentStep";
+import { useState } from 'react';
 
 export const feedbackTypes = {
     BUG: {
@@ -31,30 +29,29 @@ export const feedbackTypes = {
     },
 };
 
- export type FeedbackType = keyof typeof feedbackTypes;
+export type FeedbackType = keyof typeof feedbackTypes;
 
 export function WidgetFrom() {
     const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
 
+    function handleRestartFeedback() {
+        setFeedbackType(null);
+    }
+
     return (
         <div className="bg-zinc-900 p-4 relative rounded-2xl mb-4 flex flex-col items-center shadow-lg w-[calc(100vw-2rem)] md:w-auto">
-
-            <header>
-                <span className="text-xl leading-6">Deixe seu feedback</span>
-
-                <CloseButton />
-            </header>
-
             {!feedbackType ? (
                 <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
             ) : (
-                <p>hello World</p>
+                <FeedbackContentStep
+                 feedbackType={feedbackType}
+                 onFeedbackRestartRequested={handleRestartFeedback}
+                 />
             )}
 
             <footer className="text-xs text-neutral-400">
             Feito com ♥ por <a className="underline underline-offset-2" href="https://github.com/daniell0l">Daniel Medrado</a> 
             </footer>
-
         </div>
     );
-}
+}  
